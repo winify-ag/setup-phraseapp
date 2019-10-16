8b115575f9dff15e31514906ea43028292f9742a
Andrew Luca <thendrluca@gmail.com>
2019-10-16 12:45:43 +0300

    feat: add setup-phraseapp

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const tc = require('@actions/tool-cache');
+const os = require('os');
+const cp = require('child_process');
 
+const osMapper = {
+  win32: 'windows',
+  darwin: 'macosx',
+  linux: 'linux',
+}
 
-// most @actions toolkit packages have async methods
+const archMapper = {
+  x32: '386',
+  x64: 'amd64'
+}
+
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    const phraseapp = 'phraseapp'
+    const osPlat = os.platform();
+    const osArch = os.arch();
+    const version = core.getInput('version');
+    const cacheToolPath = tc.find(phraseapp, version)
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    if (cacheToolPath && cacheToolPath !== '') {
+      core.addPath(cacheToolPath);
+      return;
+    }
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
+    const fileName = osPlat === 'win32'
+      ? `phraseapp_${osMapper[osPlat]}_${archMapper[osArch]}.exe`
+      : `phraseapp_${osMapper[osPlat]}_${archMapper[osArch]}`
+
+    const downloadUrl = 'https://github.com/phrase/phraseapp-client/releases/download/' + version + '/' + fileName;
+    const downloadPath = await tc.downloadTool(downloadUrl);
+    const toolPath = await tc.cacheFile(downloadPath, phraseapp, phraseapp, version, osArch);
+
+    core.addPath(toolPath);
+
+    if (osPlat !== 'win32') {
+      cp.exec(`chmod +x ${toolPath}/${phraseapp}`)
+    }
+  } catch (error) {
+    core.setFailed(error);
   }
 }
 
-run()
+run();
+
